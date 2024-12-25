6c67007f98fcd9c74f90b6214acfcc8baffbca72
nakyoung <nacoon53@gmail.com>
2024-12-25 12:09:00 +0900

    init: 프로젝트 초기 셋팅

---
+++ src/main/java/com/springboot/lecture/booking/LectureBookingApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.lecture.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LectureBookingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/lecture/booking/LectureBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.lecture.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
